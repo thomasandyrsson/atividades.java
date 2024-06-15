6cda6e28ea7ac13a8ca7782b362746de69c90ed6
thomasandyrsson <130843266+thomasandyrsson@users.noreply.github.com>
2024-06-15 17:52:45 -0300

    Update Main.java

--- ATIVIDADE02/questao04/Main.java
+++ ATIVIDADE02/questao04/Main.java
@@ -8,6 +8,7 @@ public class Main {
         a1.novoContato("Aldeci Gomes", "785985155");
         a1.novoContato("Marianny Fernandes", "9877451662");
         a1.novoContato("Gabriel Oliveira", "65882544485");
+        a1.novoContato("José Florentino", "878541544448");
         a1.novoContato("João Albuquerque", "888774593");
         a1.novoContato("Teste 03", "85585545");
 
