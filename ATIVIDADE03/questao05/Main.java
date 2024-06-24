bd58b7232dddf1c34e3aaa551835fb291dc54b2e
thomasandyrsson <130843266+thomasandyrsson@users.noreply.github.com>
2024-06-24 21:20:15 +0000

    Questão 05 atualizada

--- ATIVIDADE03/questao05/Main.java
+++ ATIVIDADE03/questao05/Main.java
@@ -1,3 +1,21 @@
+/*Crie a seguinte estrutura de Herança usando a linguagem Java: (20 pontos)
+– Classe Animal (Classe MÃE)
+• Tipo (mamífero, herbívoro, ...)
+• Gênero
+– Classe Homem (Classe Filha)
+• Profissão
+– Classe Gato (Classe Filha)
+• Raça
+Faça com que as classes Homem e Gato herdem de Animal usando a palavra extends.
+Crie um método na classe MAE chamado falar(), para imprimir na tela a uma mensagem genérica: “Animal 
+falando ...”.
+Sobrescreva o método falar() nas classes filhas Homem e Gato. Na classe Homem imprima na tela “Olá boa 
+tarde!”. Na classe Gato, imprima “Miau Miau Miaaauuu ...”.
+Crie um objeto a1 da Classe Animal e chame o método falar(). Em seguida, instancie o objeto a1 para ser 
+da classe Homem e chame novamente o método falar(). Logo após, instancie o objeto a1 para ser da classe 
+Gato e chame o método falar(). Verifique que cada método falar() irá imprimir uma mensagem diferente, 
+representando a característica do polimorfismo. Ex: Animal a1 = new Animal(); a1.falar(); a1 = new Homem();
+ a1.falar(); */
 package questao05;
 
 public class Main {
@@ -7,12 +25,14 @@ public class Main {
         Animal a1 = new Gato("Mamífero", "Fêmea", "Persa");
 
         //Gato mia
+        System.out.println("O objeto a1 é um gato e o gato faz: ");
         a1.falar();
 
         //Sobrescrever gato para ser humano
         a1 = new Homem("Mamífero", "Mulher", "Gerente-geral");
 
         //Humano fala
+        System.out.println("\nAgora o a1 virou uma mulher e ela diz: ");
         a1.falar();
     }
     
